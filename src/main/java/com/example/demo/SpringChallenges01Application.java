ec91ddc773fdd486b98141a8df75e27dcd91f046
yuuuki-miyaaa <miyazaki07yuki09@gmail.com>
2025-04-04 10:19:24 +0900

    実践課題０１ファーストコミット

---
+++ src/main/java/com/example/demo/SpringChallenges01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChallenges01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChallenges01Application.class, args);
+	}
+
+}
